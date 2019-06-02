7c314a8a6a2b6c5e89bfab4a6f587b0b8c78192b
FredThx <fredthx@gmail.com>
2019-06-02 20:09:12 +0200

    Read : add output propertie

--- nodes/read.js
+++ nodes/read.js
@@ -6,12 +6,13 @@ module.exports = RED => {
 
     this.store = RED.nodes.getNode(n.store);
     this.key = n.key;
+    this.output_key = n.output_key || "payload"
 
     this.on('input', msg => {
       const key = this.key || msg.topic;
       return this.store.get(key)
         .then(data => {
-          msg.payload = data;
+          msg[this.output_key] = data;
           this.send(msg);
         })
         .catch(err => {
